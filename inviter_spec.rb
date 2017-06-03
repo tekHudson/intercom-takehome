446166186cf7e4018d8a9130f2937f2144b46ac2
Tek Hudson <tek.hudson@gmail.com>
2017-06-03 15:54:06 -0700

    Add spec for gather_invitees and sort_invitees

--- inviter_spec.rb
+++ inviter_spec.rb
@@ -7,10 +7,23 @@ describe Inviter do
     @test = Inviter.new
     @test.gather_invitees
   end
+  let (:invitee_ids) { @test.sort_invitees.map { |id, name| id } }
 
   describe "initialize" do
     it "populates customers" do
       expect(@test.customers.length).to eq(32)
+    end
+  end
+
+  describe "#gather_invitees" do
+    it "populates invitees" do
+      expect(@test.invitees.length).to eq(14)
+    end
+  end
+
+  describe "#sort_invitees" do
+    it "sorted invitees" do
+      expect(invitee_ids).to eq([4, 5, 6, 8, 11, 13, 23, 24, 26, 28, 29, 30, 31, 39])
     end
   end
 end
